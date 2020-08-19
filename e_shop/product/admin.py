b2f4b6e538a23291a6c7c45f18f0d54cdeca934e
aitiev <munarbek.aitiev@gmail.com>
2020-08-19 11:48:53 +0600

    Add product images

--- e_shop/product/admin.py
+++ e_shop/product/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from .models import Category, Product
 
+
+admin.site.register(Product)
+admin.site.register(Category)
 # Register your models here.
-#TODO admin for products
+# TODO admin for products
+# TODO select for category in products
+# TODO filtering in categories
--- e_shop/product/views.py
+++ e_shop/product/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
-#TODO products list
-#TODO product details
-#TODO add_to_cart
+from .models import Category, Product
+from .serializers import ProductSerializer, CategorySerializer
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        parent_slug = self.request.query_params.get('parent')
+        if parent_slug is not None:
+            parent_slug = parent_slug.rstrip('/')
+            queryset = queryset.filter(parent_id=parent_slug)
+        return queryset
+
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+
+# TODO: related products in category details
+# TODO add views for images
+# TODO: add pagination
+# TODO add filtration
+# TODO add_to_cart
